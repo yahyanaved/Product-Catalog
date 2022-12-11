9ae0661f6fc27879ccd39fd0cc10f7c8e4160a65
yahyanaved <yahya.naved4321@gmail.com>
2022-12-11 20:16:21 +0500

    add password hashing

--- login.php
+++ login.php
@@ -1,28 +1,30 @@
 <?php
 require 'config.php';
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = mysqli_real_escape_string($link,$_POST['username']);
+$password = mysqli_real_escape_string($link,$_POST['password']);
+$encrypted_pwd = md5($password);
 
-$sql = "SELECT user_name,passphrase,permission FROM users WHERE user_name='$username';" ;
-echo $sql;
-$result = $link->query($sql);
+$sql = "SELECT * FROM users WHERE user_name= ?;";
+if ($stmt = mysqli_prepare($link, $sql)) {
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    if (mysqli_stmt_execute($stmt)) {
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+$result = $stmt->get_result();
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $data =$row["user_name"];
+    while ($row = $result->fetch_assoc()) {
+        $data = $row["user_name"];
         $data2 = $row["passphrase"];
         $data3 = $row["permission"];
     }
 }
-if($username==$data && $password == $data2 && $data3 == 'rw')
-{
-    header('location:productadmin.php');
-}
-else if($username==$data && $password == $data2 && $data3 == 'r')
-{
+if ($username == $data && $encrypted_pwd == $data2 && $data3 == 'rw') {
+    header('location:admin.php');
+} else if ($username == $data && $data3 == 'r') {
     header('location:user.php');
-}
-else
-{
+} else {
     header('location:loginpage.php?login=0');
 }
 ?>
--- signup.php
+++ signup.php
@@ -1,9 +1,16 @@
 <?php
-    require_once "config.php"; 
-    mysqli_select_db($link,'project');
-    $username= $_POST['username'];
-    $password = $_POST['password'];
-    $data = "INSERT INTO project.users (user_name,passphrase) VALUES ('$username','$password')";
-    mysqli_query($link, $data);
-    header('location:signupsuccessfull.php')
-?>
+require_once "config.php";
+$username = $_POST['username'];
+$pwd = $_POST['password'];
+$encrypted_pwd = md5($pwd);
+$perm = "r";
+$sql = "INSERT INTO users (user_name,passphrase,permission) VALUES (?,?,?)";
+if ($stmt = mysqli_prepare($link, $sql)) {
+    mysqli_stmt_bind_param($stmt, "sss", $username, $encrypted_pwd,$perm);
+    if (mysqli_stmt_execute($stmt)) {
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+header('location:loginpage.php')
+    ?>
